522e1778b5fb68e4cc10064fc2942e096565df0e
Yuri Zinovev <yuri.zinovev@gspd.mobi>
2019-01-12 03:51:49 +0400

    task9

---
+++ #1_09.01/task9.py
@@ -0,0 +1,14 @@
+# Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого).
+
+input_str = input('Введите 3 целых через запятую: ')
+
+numbers = tuple(map(int, input_str.split(' ')))
+
+set_d = set(numbers)
+
+if len(set_d) == 3:
+    set_d.remove(max(set_d))
+    set_d.remove(min(set_d))
+    print("среднее значение это %s" % set_d.pop())
+else:
+    print("слишком мало уникальных значений")

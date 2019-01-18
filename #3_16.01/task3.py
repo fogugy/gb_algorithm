98bb20f8ba546d5780fec5ddf50bb7eee1e38b5f
Yuri Zinovev <yuri.zinovev@gspd.mobi>
2019-01-19 00:27:18 +0400

    hw3/ task3

---
+++ #3_16.01/task3.py
@@ -0,0 +1,26 @@
+# В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
+
+import random as rnd
+
+ls = [rnd.randint(0, 100) for x in range(10)]
+
+max_nmb = ls[0]
+min_nmb = ls[0]
+max_idx = -1
+min_idx = -1
+
+print(ls)
+
+for idx, nmb in enumerate(ls):
+    if nmb > max_nmb:
+        max_nmb = nmb
+        max_idx = idx
+    elif nmb < min_nmb:
+        min_nmb = nmb
+        min_idx = idx
+
+print("max:", max_nmb, "min:", min_nmb)
+
+ls[min_idx], ls[max_idx] = ls[max_idx], ls[min_idx]
+
+print(ls)

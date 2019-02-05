20c98862a6994e0a66781d60e4842d4d9a8ce6c2
Yuri Zinovev <yuri.zinovev@gspd.mobi>
2019-02-05 15:10:37 +0400

    hw8/ task1 - array of includes

--- #8_02.02/task1.py
+++ #8_02.02/task1.py
@@ -9,15 +9,16 @@ def rabin_karp(src: str, query: str):
     m = len(query)
     hquery = hash(query)
     hsrc = hash(src[1:m])
+    indexes = []
     for i in range(n-m+1):
         if hquery == hsrc:
             if src[i:i+m] == query:
-                return i
+                indexes.append(i)
         hsrc = hash(src[i+1:i+m+1])
-    return -1
+    return indexes if len(indexes) > 0 else -1
 
 
-src = 'abcdefghijklmnopqrstuvwxwz'
+src = 'abcdefghijklmdefnopqrstuvwxwzdef'
 
 print(rabin_karp(src, 'def'))
 print(rabin_karp(src, 'zzz'))
